152a59421b2275ea5aafaf3ebee1372e76a5e91f
Tolriq <tolriq@tolriq.com>
2020-11-16 11:50:11 +0100

    Simplify + no Timber

--- tracker/src/main/java/org/matomo/sdk/extra/MatomoExceptionHandler.kt
+++ tracker/src/main/java/org/matomo/sdk/extra/MatomoExceptionHandler.kt
@@ -6,11 +6,9 @@
  */
 package org.matomo.sdk.extra
 
-import org.matomo.sdk.Matomo
 import org.matomo.sdk.TrackMe
 import org.matomo.sdk.Tracker
 import org.matomo.sdk.dispatcher.DispatchMode
-import timber.log.Timber
 
 /**
  * An exception handler that wraps the existing exception handler and dispatches event to a [org.matomo.sdk.Tracker].
@@ -19,13 +17,7 @@ import timber.log.Timber
  * Also see documentation for [TrackHelper.uncaughtExceptions]
  */
 class MatomoExceptionHandler(val tracker: Tracker, private val mTrackMe: TrackMe?) : Thread.UncaughtExceptionHandler {
-    private val mDefaultExceptionHandler: Thread.UncaughtExceptionHandler? = Thread.getDefaultUncaughtExceptionHandler()
-
-    /**
-     * This will give you the previous exception handler that is now wrapped.
-     */
-    private val defaultExceptionHandler: Thread.UncaughtExceptionHandler?
-        get() = mDefaultExceptionHandler
+    private val defaultExceptionHandler: Thread.UncaughtExceptionHandler? = Thread.getDefaultUncaughtExceptionHandler()
 
     override fun uncaughtException(thread: Thread, ex: Throwable) {
         try {
@@ -38,18 +30,11 @@ class MatomoExceptionHandler(val tracker: Tracker, private val mTrackMe: TrackMe
 
             // Immediately dispatch as the app might be dying after rethrowing the exception and block until the dispatch is completed
             tracker.dispatchBlocking()
-        } catch (e: Exception) {
-            Timber.tag(TAG).e(e, "Couldn't track uncaught exception")
         } finally {
             // re-throw critical exception further to the os (important)
             if (defaultExceptionHandler != null && defaultExceptionHandler !== this) {
-                defaultExceptionHandler!!.uncaughtException(thread, ex)
+                defaultExceptionHandler.uncaughtException(thread, ex)
             }
         }
     }
-
-    companion object {
-        private val TAG = Matomo.tag(MatomoExceptionHandler::class.java)
-    }
-
 }
