0d5ec650f64717b3a524653f0b4194e6cd7498dd
Tolriq <tolriq@tolriq.com>
2020-11-16 12:23:44 +0100

    Minsdk 21

--- tracker/src/main/java/org/matomo/sdk/tools/DeviceHelper.kt
+++ tracker/src/main/java/org/matomo/sdk/tools/DeviceHelper.kt
@@ -6,9 +6,7 @@
  */
 package org.matomo.sdk.tools
 
-import android.annotation.TargetApi
 import android.content.Context
-import android.os.Build
 import android.util.DisplayMetrics
 import android.view.Display
 import android.view.WindowManager
@@ -42,26 +40,21 @@ class DeviceHelper(private val mContext: Context, private val mPropertySource: P
                 val android = mBuildInfo.release
                 val model = mBuildInfo.model
                 val build = mBuildInfo.buildId
-                httpAgent = String.format(Locale.US,
-                        "Dalvik/%s (Linux; U; Android %s; %s Build/%s)",
-                        dalvik, android, model, build
-                )
+                httpAgent = String.format(Locale.US, "Dalvik/%s (Linux; U; Android %s; %s Build/%s)", dalvik, android, model, build)
             }
             return httpAgent
-        }// This is not accurate on all 4.2+ devices, usually the height is wrong due to statusbar/softkeys
-    // Better than nothing though.
-// Reflection bad, still this is the best way to get an accurate screen size on API14-16.// Recommended way to get the resolution but only available since API17
+        }
+
     /**
      * Tries to get the most accurate device resolution.
      * On devices below API17 resolution might not account for statusbar/softkeys.
      *
      * @return [width, height]
      */
-    @get:TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
     val resolution: IntArray?
         get() {
-            var width = -1
-            var height = -1
+            var width: Int
+            var height: Int
             val display: Display
             display = try {
                 val wm = mContext.getSystemService(Context.WINDOW_SERVICE) as WindowManager
@@ -70,27 +63,15 @@ class DeviceHelper(private val mContext: Context, private val mPropertySource: P
                 Timber.tag(TAG).e(e, "Window service was not available from this context")
                 return null
             }
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-                // Recommended way to get the resolution but only available since API17
-                val dm = DisplayMetrics()
-                display.getRealMetrics(dm)
-                width = dm.widthPixels
-                height = dm.heightPixels
-            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
-                // Reflection bad, still this is the best way to get an accurate screen size on API14-16.
-                try {
-                    val getRawWidth = Display::class.java.getMethod("getRawWidth")
-                    val getRawHeight = Display::class.java.getMethod("getRawHeight")
-                    width = getRawWidth.invoke(display) as Int
-                    height = getRawHeight.invoke(display) as Int
-                } catch (e: Exception) {
-                    Timber.tag(TAG).w(e, "Reflection of getRawWidth/getRawHeight failed on API14-16 unexpectedly.")
-                }
-            }
+            // Recommended way to get the resolution but only available since API17
+            var dm = DisplayMetrics()
+            display.getRealMetrics(dm)
+            width = dm.widthPixels
+            height = dm.heightPixels
             if (width == -1 || height == -1) {
                 // This is not accurate on all 4.2+ devices, usually the height is wrong due to statusbar/softkeys
                 // Better than nothing though.
-                val dm = DisplayMetrics()
+                dm = DisplayMetrics()
                 display.getMetrics(dm)
                 width = dm.widthPixels
                 height = dm.heightPixels
